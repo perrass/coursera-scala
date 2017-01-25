a691a52acc0ce35b3a627726a28e57567f19d419
perrass <pixelss@163.com>
2017-01-25 09:23:14 +0800

    perra

--- Course01FPPIS/Week2/Rational.scala
+++ Course01FPPIS/Week2/Rational.scala
@@ -4,6 +4,9 @@ package Course01FPPIS.Week2
   * Created by perra on 17-1-23.
   */
 class Rational(x: Int, y: Int) {
+  require(y > 0, "denominator must be positive")
+
+  def this(x: Int) = this(x, 1)
 
   private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
   private val g = gcd(x, y)
@@ -11,19 +14,19 @@ class Rational(x: Int, y: Int) {
   def numer = x / g
   def denom = y / g
 
-  def add(that: Rational) =
+  def + (that: Rational) =
     new Rational(
       numer * that.denom + denom * that.numer,
       denom * that.denom
     )
 
-  def neg: Rational = new Rational(-numer, denom)
+  def unary_- : Rational = new Rational(-numer, denom)
 
-  def sub(that: Rational) = add(that.neg)
+  def - (that: Rational) = this + -that
 
-  def less(that: Rational) = numer * that.denom < that.numer * denom
+  def < (that: Rational) = numer * that.denom < that.numer * denom
 
-  def max(that: Rational) = if (this less that) that else this
+  def max(that: Rational) = if (this < that) that else this
 
   override def toString() = numer + "/" + denom
 
